f5f8c99d3c1e486d5199f4e5e52dfcdadc952a7c
OCHOA ORTIZ, HECTOR <719509@celes.unizar.es>
2019-04-06 15:56:43 +0200

    Merge rama geocode con diseño-gestion-BD, añadir documentación en castellano para la geocodificación

---
+++ app/main/controller/geocode_controller.py
@@ -0,0 +1,32 @@
+from flask_restplus import Resource
+
+from ..util.dto import GeocodeDto
+from ..service.geocode_service import point_to_address, address_to_point
+
+api = GeocodeDto.api
+
+
+@api.route('/address/<float:lat>/<float:lon>')
+@api.param('lat', 'Latitud')
+@api.param('lon', 'Longitud')
+class PointToAddress(Resource):
+    @api.doc('point')
+    @api.response(200, 'OK.')
+    @api.response(400, 'Error genérico.')
+    @api.response(404, 'Localización no encontrada.')
+    @api.doc('address')
+    def get(self, lat, lon):
+        """Devuelve una lista de direcciones dadas latitud y longitud (en sistema de referencia WGS84)."""
+        return point_to_address(lat, lon)
+
+
+@api.route('/point/<string:address>')
+@api.param('address', 'Dirección')
+class AddressToPoint(Resource):
+    @api.doc('point')
+    @api.response(200, 'OK.')
+    @api.response(400, 'Error genérico.')
+    @api.response(404, 'Localización no encontrada.')
+    def get(self, address):
+        """Devuelve un punto en sistema de referencia WGS84 dada una dirección."""
+        return address_to_point(address)

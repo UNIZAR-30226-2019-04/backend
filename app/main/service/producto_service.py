c3836d6928b4ee9900b28db00eefd934a810e0be
DanFzgz <fraile.daniel@yahoo.es>
2019-04-11 18:35:52 +0200

    Búsqueda básica de producto

--- app/main/service/producto_service.py
+++ app/main/service/producto_service.py
@@ -1,4 +1,5 @@
 from geoalchemy2 import WKTElement
+from sqlalchemy import text
 from app.main.model.producto import Producto
 from app.main.model.pertenece import Pertenece
 from .. import db
@@ -50,6 +51,42 @@ def get_a_product(id_producto):
             'message': 'Product not found.',
         }
         return response_object, 404
+
+
+# TODO: Completar con los parámetros que se quiera (los joins requieren trabajo adicional)
+# def search_products(textoBusqueda, preciomin, preciomax, ubicacion, radioUbicacion, valoracionMin, valoracionMax):
+def search_products(textobusqueda=None, preciomin=None, preciomax=None):
+    query_args = {}
+    query = "SELECT * FROM \"Producto\" WHERE "
+    numpars = 0
+    # TODO: Pensar bien cómo hacer esta búsqueda, ¿número de apariciones del string?, ¿tiene más peso si aparece en el título?, ...
+    if textobusqueda:
+        textobusqueda = "%" + textobusqueda + "%"
+        query += "(titulo LIKE :textobusqueda OR descripcion LIKE :textobusqueda)"
+        numpars += 1
+        query_args['textobusqueda'] = textobusqueda
+    if preciomin:
+        if numpars != 0:
+            query += " AND "
+        query += "\"precioBase\" >= :preciomin"
+        numpars += 1
+        query_args['preciomin'] = preciomin
+    if preciomax:
+        if numpars != 0:
+            query += " AND "
+        query += "\"precioBase\" <= :preciomax"
+        numpars += 1
+        query_args['preciomax'] = preciomax
+
+    result = db.engine.execute(text(query), query_args)
+    d, a = {}, []
+    for row in result:
+        # row.items() returns an array like [(key0, value0), (key1, value1)]
+        for column, value in row.items():
+            # build up the dictionary
+            d = {**d, **{column: value}}
+        a.append(d)
+    return a
 
 
 def get_product_categories(id_producto):

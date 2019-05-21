3ad3dc938864fd753011db2f2559c5f23ed71c40
DanFzgz <fraile.daniel@yahoo.es>
2019-05-21 18:51:14 +0100

    URL confirmar mail

--- app/main/service/generar_email.py
+++ app/main/service/generar_email.py
@@ -15,7 +15,7 @@ def generateEmail(user, token):
     tag_nombre = soup.find(id="nombre_usuario")
     tag_nombre.string.replace_with("Hola " + user.nick + ", ")
     # TODO: Cambiar a https cuando sea necesario
-    url_token = "http://155.210.47.51:5000/user/" + user.public_id + "/confirmar_email/" + token.decode()
+    url_token = "http://34.90.77.95:5000/user/" + user.public_id + "/confirmar_email/" + token.decode()
     button = soup.find(id="button")
     button['href'] = url_token
     a_token = soup.find(id="url_token")
